56f1fb164c290df613628eda43b81d1194c74e7f
Glavo <zjx001202@126.com>
2017-10-01 14:38:43 +0800

    Auto commit by Glavo

--- stl/src/exception.cpp
+++ stl/src/exception.cpp
@@ -6,85 +6,87 @@
 #include <cstdio>
 #include <cstdlib>
 
+#ifdef __GNUC__
+
+#include "linux/exception.tcc"
+
+#endif
+
 #pragma clang diagnostic push
+#pragma ide diagnostic ignored "UnusedImportStatement"
 #pragma ide diagnostic ignored "ClangTidyInspection"
-using namespace std;
 
-static void default_unexpected_handle() {
-    fprintf(stderr, "terminating");
-    exit(6);
-}
+namespace std {
+    static int nexception;
+    static void *exception_stack;
 
-static unexpected_handler uh = default_unexpected_handle;
-
-static terminate_handler th = default_unexpected_handle;
-
-char const *std::exception::what() const noexcept {
-    return "std::exception";
-}
-
-char const *std::bad_exception::what() const noexcept {
-    return "std::bad_exception";
-}
-
-unexpected_handler std::get_unexpected() noexcept {
-    return uh;
-}
-
-unexpected_handler std::set_unexpected(unexpected_handler f) noexcept {
-    auto old = uh;
-    uh = f;
-    return old;
-}
-
-[[noreturn]] void std::unexpected() {
-    try {
-        uh();
-        // handler should not return
-        fprintf(stderr,
-                "terminate_handler unexpectedly returned\n");
-        ::abort();
+    static void default_unexpected_handle() {
+        fprintf(stderr, "terminating");
+        exit(6);
     }
-    catch (...) {
-        fprintf(stderr,
-                "terminate_handler unexpectedly threw an exception\n");
-        ::abort();
+
+    static unexpected_handler uh = default_unexpected_handle;
+
+    static terminate_handler th = default_unexpected_handle;
+
+    unexpected_handler get_unexpected() noexcept {
+        return uh;
+    }
+
+    unexpected_handler set_unexpected(unexpected_handler f) noexcept {
+        auto old = uh;
+        uh = f;
+        return old;
+    }
+
+    [[noreturn]] void unexpected() {
+        try {
+            uh();
+            // handler should not return
+            fprintf(stderr,
+                    "terminate_handler unexpectedly returned\n");
+            ::abort();
+        }
+        catch (...) {
+            fprintf(stderr,
+                    "terminate_handler unexpectedly threw an exception\n");
+            ::abort();
+        }
+    }
+
+    terminate_handler get_terminate() noexcept {
+        return th;
+    }
+
+    terminate_handler set_terminate(terminate_handler f) noexcept {
+        auto old = th;
+        th = f;
+        return th;
+    }
+
+    void terminate() noexcept {
+        try {
+            th();
+            // handler should not return
+            fprintf(stderr,
+                    "terminate_handler unexpectedly returned\n");
+            ::abort();
+        }
+        catch (...) {
+            fprintf(stderr,
+                    "terminate_handler unexpectedly threw an exception\n");
+            ::abort();
+        }
+    }
+
+    int uncaught_exceptions() noexcept {
+        return nexception;
+    }
+
+    bool std::uncaught_exception() noexcept {
+        return std::uncaught_exceptions() > 0;
     }
 }
-
-terminate_handler std::get_terminate() noexcept {
-    return th;
-}
-
-terminate_handler std::set_terminate(terminate_handler f) noexcept {
-    auto old = th;
-    th = f;
-    return th;
-}
-
-void std::terminate() noexcept {
-    try {
-        th();
-        // handler should not return
-        fprintf(stderr,
-                "terminate_handler unexpectedly returned\n");
-        ::abort();
-    }
-    catch (...) {
-        fprintf(stderr,
-                "terminate_handler unexpectedly threw an exception\n");
-        ::abort();
-    }
-}
-
-int std::uncaught_exceptions() noexcept {
-    ::abort();
-}
-
-bool std::uncaught_exception() noexcept {
-    return std::uncaught_exceptions() > 0;
-}
-
 
 
 #pragma clang diagnostic pop
--- stl/src/linux/exception.tcc
+++ stl/src/linux/exception.tcc
@@ -3,6 +3,9 @@
 //
 #include <cstdlib>
 
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "ClangTidyInspection"
+
 extern "C"
 void *__cxa_allocate_exception(size_t thrown_size) noexcept {
     return malloc(thrown_size);
@@ -12,3 +15,6 @@ extern "C"
 void __cxa_free_exception(void *thrown_exception) noexcept {
     free(thrown_exception);
 }
+
+
+#pragma clang diagnostic pop
